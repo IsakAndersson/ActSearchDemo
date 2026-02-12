7d12d7e1d0874d528e18f804b0385d72881b1a15
antr0380 <70625831+antr0380@users.noreply.github.com>
2026-02-12 05:06:25 +0100

    Utökad dummy evaluation

--- evaluation/qrels.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-qrels = pd.DataFrame([
-    {'query_id': "Q0", 'doc_id': "D0", 'relevance': 0},
-    {'query_id': "Q0", 'doc_id': "D1", 'relevance': 1},
-    {'query_id': "Q1", 'doc_id': "D0", 'relevance': 0},
-    {'query_id': "Q1", 'doc_id': "D3", 'relevance': 2},
-])
--- evaluation/run.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-run = pd.DataFrame([
-    {'query_id': "Q0", 'doc_id': "D0", 'score': 1.2},
-    {'query_id': "Q0", 'doc_id': "D1", 'score': 1.0},
-    {'query_id': "Q1", 'doc_id': "D0", 'score': 2.4},
-    {'query_id': "Q1", 'doc_id': "D3", 'score': 3.6},
-])
