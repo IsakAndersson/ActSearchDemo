d4608d72889701bf0d753eef93e5659e2d59fe0f
antr0380 <70625831+antr0380@users.noreply.github.com>
2026-02-15 23:43:18 +0100

    Städa upp + tester

--- evaluation/evaluation.py
+++ evaluation/evaluation.py
@@ -1,37 +1,70 @@
 import ir_measures
 from ir_measures import *
 import pandas as pd
+from typing import Callable, List, Tuple
 
-def main():
+""" 
+Format för run:
+run = pd.DataFrame([
+    {'query_id': "Q0", 'doc_id': "D0", 'score': 1.2},
+    {'query_id': "Q0", 'doc_id': "D1", 'score': 1.0},
+    {'query_id': "Q1", 'doc_id': "D0", 'score': 2.4},
+    {'query_id': "Q1", 'doc_id': "D3", 'score': 3.6},
+])"""
 
+def evaluate_system(search_function, k):
 	df = load_data()
 
 	#tar ut rätt kolumner för qrels 
 	doc_ids = df["Titel på rätt dokument"]
-	query_type_list = ["Case-beskrivning", "Titel på rätt dokument", "Titel utan stor bokstav", "2 nyckelord", "2 nyckelord med stavfel", "2 nyckelord med synonymer", "Längre sökterm"]
+	query_type_list = ["Case-beskrivning", "Titel på rätt dokument", "Titel utan stor bokstav", "2 nyckelord", 
+                       "2 nyckelord med stavfel", "2 nyckelord med synonymer", "Längre sökterm"]
 	query_types_cols = df[query_type_list]
 
-	aggregate_results_list = []
-	average_rank_list = []
+	#todo: lägg in for-loop som loopar igenom varje sökfunktion och kör evaluate
+	results, average_rank, average_score = evaluate(search_function, k, doc_ids, query_types_cols)
+	print_results(results, average_rank, average_score)
+	return results, average_rank, average_score
 
-	#loopar över varje query type, skapar qrels och test-run och beräknar metrics 
+def evaluate(search_function, k, doc_ids, query_types_cols):
+	rows = []
+	#loopar över varje query type och beräknar metrics 
 	for query_type in query_types_cols.columns:
-		qrels = create_qrels(doc_ids, query_types_cols[query_type])
-		run = create_test_run(doc_ids, query_types_cols[query_type])
+		queries = query_types_cols[query_type]
+		rr20, average_rank = evaluate_query_type(doc_ids, queries, search_function, k)
+		rows.append({"query_type": query_type, "RR@20": rr20, "average_rank": average_rank})
 		
-		scores = calculate_metrics(qrels, run)
-		aggregate_results_list.append(list(scores.values())[0]) 
+	results_by_query_type = pd.DataFrame(rows)
+	average_score = results_by_query_type["RR@20"].mean()
+	average_rank = results_by_query_type["average_rank"].mean()
+ 
+	return (results_by_query_type, average_rank, average_score)
 
-		average_rank = calculate_average_rank(qrels, run)
-		average_rank_list.append(average_rank)
-		
-	rows = zip(query_type_list, aggregate_results_list, average_rank_list)
-	results = pd.DataFrame(rows, columns=['query_type', 'RR@20', 'average_rank'])
- 
-	average_score_df = results.iloc[:, 1].mean() #hårdkodat för RR@20 just nu
-	average_rank = results.iloc[:, 2].mean() 
- 
-	print_results(results, average_score_df, average_rank)
+def evaluate_query_type(doc_ids, queries, search_function, k):
+    qrels = create_qrels(doc_ids, queries)
+    run = build_run_df(queries, search_function, k)
+    scores = calculate_metrics(qrels, run)
+    rr20 = list(scores.values())[0]
+    avg_rank = calculate_average_rank(qrels, run)
+    return rr20, avg_rank
+
+def build_run_df(
+    queries: pd.Series,
+    search_fn: Callable[[str, int], List[Tuple[str, float]]],
+    top_k: int = 20
+) -> pd.DataFrame:
+    rows = []
+    for query in queries:
+        results = search_fn(query, top_k)
+        # results = [(doc_id, score), ...]
+        for doc_id, score in results:
+            rows.append({
+                "doc_id": str(doc_id),   # titel
+                "query_id": query,
+                "score": float(score)
+            })
+    run = pd.DataFrame(rows)
+    return run
 
 def load_data():
     #Läser in data från Google Sheets: https://docs.google.com/spreadsheets/d/1_bjtkzZyc-59dAf8c0LlnNFqXl9rB01LGJF3HFoKo6I/edit?usp=sharing
@@ -62,37 +95,11 @@ def create_qrels(doc_ids, query_ids):
 	})
 	return qrels
 
-""" 
-Den här funktionen är bara för testning
-
-Format för run:
-run = pd.DataFrame([
-    {'query_id': "Q0", 'doc_id': "D0", 'score': 1.2},
-    {'query_id': "Q0", 'doc_id': "D1", 'score': 1.0},
-    {'query_id': "Q1", 'doc_id': "D0", 'score': 2.4},
-    {'query_id': "Q1", 'doc_id': "D3", 'score': 3.6},
-])"""
-def create_test_run(query_id, doc_id):
-	scores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-	run = pd.DataFrame({
-		"query_id": query_id,
-		"doc_id": doc_id,
-		"score": scores
-	})
-	
-	run.to_csv('run_test.csv', index=False)
- 
-	return run
-
 """
-Beräknar metrics
+Beräknar metrics, returnerar en python dictionary
 
 MRR@20: För varje query beräknas: 1/positionen för det rätta dokumentet. 0 poäng om dokumentet hamnar utanför topp 20.
 		Tar snittet över alla queries. 
-    	
-
-Returns a python dictionary 
 """
 def calculate_metrics(qrels, run):
 	metrics = [RR@20]
@@ -112,7 +119,7 @@ def calculate_average_rank(qrels, run):
 	avg_rank = sum(ranks) / len(ranks) if ranks else 0
 	return avg_rank
 
-def print_results(results, average_score, average_rank_df):
+def print_results(results, average_score, average_rank):
 	BOLD = '\033[1m'
 	UNDERLINE = '\033[4m'
 	END = '\033[0m'
@@ -123,8 +130,8 @@ def print_results(results, average_score, average_rank_df):
 	print("\nModell: " + model + "\n")
 	print(results.to_string(index=False))
 	print("\n Genomsnittlig RR@20: " + str(average_score))
-	print(" Genomsnittlig position av rätt dokument (average rank): " + str(average_rank_df) + "\n")
-	print(UNDERLINE + "Förklaring av mått:" + END + "\nRR@20 (Mean Reciprocal Rank): Mäter hur högt upp i resultatlistan det rätta svaret placeras i topp 20. Beräknas som 1/positionen för det rätta dokumentet. \n1.0 = Perfekt score (högst upp varje gång) \n0.0 = Utanför topp 20 varje gång\n")
- 
-if __name__ == "__main__":
-    main()
+	print(" Average rank: " + str(average_rank) + "\n")
+	print(UNDERLINE + "Förklaring av mått:" + END 
+       + "\nRR@20 (Mean Reciprocal Rank): Mäter hur högt upp i resultatlistan det rätta svaret placeras i topp 20. Beräknas som 1/positionen för det rätta dokumentet. \n1.0 = Perfekt score (högst upp varje gång) \n0.0 = Utanför topp 20 varje gång\n"
+       + "\nAverage rank: Genomsnittlig position av rätt dokument. Ignorerar resultat utanför top k"
+       )
